1de3459da653e09c8c824bbb83355e64460a7326
Amitay Horwitz <amitayh@gmail.com>
2013-04-23 09:09:34 +0300

    Decoupled XML config file validation from console

---
+++ gofish/config/XMLConfigFactory.java
@@ -0,0 +1,158 @@
+package gofish.config;
+
+import gofish.Config;
+import gofish.ConfigFactory;
+import gofish.exception.ConfigValidationException;
+import gofish.model.Card;
+import gofish.model.Player;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.validation.SchemaFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+abstract public class XMLConfigFactory implements ConfigFactory {
+    
+    final private static String SCHEMA_FILE = "gofish/resources/gofish.xsd";
+    
+    final private static String TYPE_COMPUTER = "COMPUTER";
+    
+    final private static String TYPE_HUMAN = "HUMAN";
+    
+    private File file;
+    
+    private Element root;
+    
+    private Config config;
+    
+    @Override
+    public Config getConfig() {
+        return config;
+    }
+    
+    public void validate(File file) throws ConfigValidationException {
+        this.file = file;
+        validateFile();
+        setRootElement();
+        createConfig();
+        config.validate();
+    }    
+    
+    private void validateFile() {
+        if (!file.exists() || !file.isFile()) {
+            throw new ConfigValidationException("File does not exist");
+        }
+    }
+    
+    private void setRootElement() {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setIgnoringElementContentWhitespace(true);
+        factory.setIgnoringComments(true);
+        factory.setNamespaceAware(true);
+        
+        DocumentBuilder builder;
+        try {
+            // XSD schema validation
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            URL url = this.getClass().getClassLoader().getResource(SCHEMA_FILE);
+            factory.setSchema(schemaFactory.newSchema(url));
+            builder = factory.newDocumentBuilder();
+            builder.setErrorHandler(new DefaultHandler()); // Suppress warnings
+        } catch (SAXException | ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+        
+        try {
+            Document doc = builder.parse(file);
+            root = doc.getDocumentElement();
+        } catch (SAXException e) {
+            String message = "Schema validation failed - " + e.getMessage();
+            throw new ConfigValidationException(message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private void createConfig() {
+        config = new Config();
+        
+        Node settings = root.getElementsByTagName("settings").item(0);
+        for (Node node = settings.getFirstChild(); node != null; node = node.getNextSibling()) {
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                switch (node.getNodeName()) {
+                    case "allowMutipleRequests":
+                        config.setAllowMutipleRequests(getBooleanValue(node));
+                        break;
+                    case "forceShowOfSeries":
+                        config.setForceShowOfSeries(getBooleanValue(node));
+                        break;
+                }
+            }
+        }
+        
+        Node players = root.getElementsByTagName("players").item(0);
+        for (Node node = players.getFirstChild(); node != null; node = node.getNextSibling()) {
+            if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("player")) {
+                Player player = getPlayer((Element) node);
+                config.addPlayer(player);
+            }
+        }
+    }
+    
+    private Player getPlayer(Element playerElement) {
+        Player player = null;
+        String name = playerElement.getAttribute("name");
+        String type = playerElement.getAttribute("type");
+        switch (type) {
+            case TYPE_COMPUTER:
+                player = createPlayer(Player.Type.COMPUTER, name);
+                break;
+            case TYPE_HUMAN:
+                player = createPlayer(Player.Type.HUMAN, name);
+                break;
+            default:
+                throw new ConfigValidationException("Unexpected player type - " + type);
+        }
+        
+        if (player != null) {
+            Node cards = playerElement.getElementsByTagName("cards").item(0);
+            for (Node node = cards.getFirstChild(); node != null; node = node.getNextSibling()) {
+                if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("card")) {
+                    Card card = getCard((Element) node);
+                    player.addCard(card);
+                }
+            }
+        }
+        
+        return player;
+    }
+    
+    private Card getCard(Element cardElement) {
+        String name = cardElement.getAttribute("name");
+        List<String> properties = new ArrayList<>();
+        for (Node node = cardElement.getFirstChild(); node != null; node = node.getNextSibling()) {
+            if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("series")) {
+                properties.add(node.getTextContent());
+            }
+        }
+        
+        return new Card(name, properties);
+    }
+    
+    private boolean getBooleanValue(Node node) {
+        return node.getTextContent().equals("true");
+    }
+    
+    abstract protected Player createPlayer(Player.Type type, String name);
+
+}
--- gofish/console/config/XMLConfig.java
+++ gofish/console/config/XMLConfig.java
@@ -2,35 +2,15 @@ package gofish.console.config;
 
 import gofish.Config;
 import gofish.ConfigFactory;
+import gofish.config.XMLConfigFactory;
 import gofish.console.player.Computer;
 import gofish.console.player.Human;
 import gofish.exception.ConfigValidationException;
-import gofish.model.Card;
 import gofish.model.Player;
 import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.validation.SchemaFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 public class XMLConfig implements ConfigFactory {
-    
-    final private static String SCHEMA_FILE = "gofish/resources/gofish.xsd";
-    
-    final private static String TYPE_COMPUTER = "COMPUTER";
-    
-    final private static String TYPE_HUMAN = "HUMAN";
     
     private Scanner input;
 
@@ -40,13 +20,14 @@ public class XMLConfig implements ConfigFactory {
 
     @Override
     public Config getConfig() {
+        XMLConfigFactory factory = new Factory();
         boolean isValid = false;
         Config config = null;
         
         do {            
             try {
-                config = createConfig();
-                config.validate();
+                factory.validate(getFile());
+                config = factory.getConfig();
                 isValid = true;
             } catch (ConfigValidationException e) {
                 System.out.println("Invalid config file - " + e.getMessage());
@@ -54,71 +35,6 @@ public class XMLConfig implements ConfigFactory {
         } while (!isValid);
         
         return config;
-    }
-    
-    private Config createConfig() {
-        Config config = new Config();
-        
-        Element root = getXml();
-        
-        Node settings = root.getElementsByTagName("settings").item(0);
-        for (Node node = settings.getFirstChild(); node != null; node = node.getNextSibling()) {
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                switch (node.getNodeName()) {
-                    case "allowMutipleRequests":
-                        config.setAllowMutipleRequests(getBooleanValue(node));
-                        break;
-                    case "forceShowOfSeries":
-                        config.setForceShowOfSeries(getBooleanValue(node));
-                        break;
-                }
-            }
-        }
-        
-        Node players = root.getElementsByTagName("players").item(0);
-        for (Node node = players.getFirstChild(); node != null; node = node.getNextSibling()) {
-            if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("player")) {
-                Player player = getPlayer((Element) node);
-                config.addPlayer(player);
-            }
-        }
-        
-        return config;
-    }
-    
-    private Element getXml() {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        factory.setIgnoringElementContentWhitespace(true);
-        factory.setIgnoringComments(true);
-        factory.setNamespaceAware(true);
-        
-        DocumentBuilder builder;
-        try {
-            // XSD schema validation
-            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            URL url = this.getClass().getClassLoader().getResource(SCHEMA_FILE);
-            factory.setSchema(schemaFactory.newSchema(url));
-            builder = factory.newDocumentBuilder();
-            builder.setErrorHandler(new DefaultHandler()); // Suppress warnings
-        } catch (SAXException | ParserConfigurationException e) {
-            throw new RuntimeException(e);
-        }
-        
-        boolean isValid = false;
-        Element root = null;
-        do {
-            try {
-                Document doc = builder.parse(getFile());
-                root = doc.getDocumentElement();
-                isValid = true;
-            } catch (SAXException e) {
-                System.out.println("Schema validation failed - " + e.getMessage());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        } while (!isValid);
-        
-        return root;
     }
     
     private File getFile() {
@@ -131,48 +47,24 @@ public class XMLConfig implements ConfigFactory {
         return file;
     }
     
-    private Player getPlayer(Element playerElement) {
-        Player player = null;
-        String name = playerElement.getAttribute("name");
-        String type = playerElement.getAttribute("type");
-        switch (type) {
-            case TYPE_COMPUTER:
-                player = new Computer(name);
-                break;
-            case TYPE_HUMAN:
-                player = new Human(input, name);
-                break;
-            default:
-                throw new ConfigValidationException("Unexpected player type - " + type);
-        }
-        
-        if (player != null) {
-            Node cards = playerElement.getElementsByTagName("cards").item(0);
-            for (Node node = cards.getFirstChild(); node != null; node = node.getNextSibling()) {
-                if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("card")) {
-                    Card card = getCard((Element) node);
-                    player.addCard(card);
-                }
+    private class Factory extends XMLConfigFactory {
+
+        @Override
+        protected Player createPlayer(Player.Type type, String name) {
+            Player player;
+            switch (type) {
+                case COMPUTER: 
+                    player = new Computer(name);
+                    break;
+                case HUMAN:
+                    player = new Human(input, name);
+                    break;
+                default:
+                    throw new RuntimeException("Unexpected player type");
             }
+            return player;
         }
         
-        return player;
-    }
-    
-    private Card getCard(Element cardElement) {
-        String name = cardElement.getAttribute("name");
-        List<String> properties = new ArrayList<>();
-        for (Node node = cardElement.getFirstChild(); node != null; node = node.getNextSibling()) {
-            if (node.getNodeType() == Node.ELEMENT_NODE && node.getNodeName().equals("series")) {
-                properties.add(node.getTextContent());
-            }
-        }
-        
-        return new Card(name, properties);
-    }
-    
-    private boolean getBooleanValue(Node node) {
-        return node.getTextContent().equals("true");
     }
 
 }
